6395621b102101ac753b299ea4cf10b917385c43
Caleb Pendergraft <cackmed@gmail.com>
2020-11-19 08:19:25 -0800

    got basic infrastructure set up

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function App() {
   return (
-    <>
-      <h1>
+    
+    <h1>
           Hello World
-      </h1>
-    </>
+    </h1>
+    
   );
 }
---
+++ src/service/fetchApi.js
@@ -0,0 +1,20 @@
+const request = (path, method, body) => {
+  return fetch(`${process.env.LOCAL_HOST}${path}`, {
+    method,
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  })
+    .then(res => Promise.all([res.ok, res.json()]))
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json;
+    });
+};
+  
+export const post = (path, body) => request(path, 'POST', body);
+export const get = path => request(path, 'GET');
+export const patch = (path, body) => request(path, 'PATCH', body);
+export const del = path => request(path, 'DELETE');
